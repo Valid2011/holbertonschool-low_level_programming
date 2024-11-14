634ba134c61ad717222666ef688ba30883e0cfbb
git config --global user.name Your <orujovvalid2020@gmail.com>
2024-11-14 15:20:01 +0400

    a

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);  /* Declaration of the function */
+void print_alphabet_x10(void);
+int _islower(int c);  /* Add this line to declare the function */
 
 #endif /* MAIN_H */
