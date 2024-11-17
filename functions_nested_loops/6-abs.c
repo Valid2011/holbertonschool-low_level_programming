41836e60784788737414a03d25391220e3e60bd8
git config --global user.name Your <orujovvalid2020@gmail.com>
2024-11-17 18:01:35 +0400

    a

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -8,8 +8,8 @@
  */
 int _abs(int n)
 {
-    if (n < 0)
-        return (-n);  /* Return the negation of n if it's negative */
-    else
-        return (n);   /* Return n if it's non-negative */
+	if (n < 0)
+		return (-n);  /* Return the negation of n if it's negative */
+	else
+		return (n);   /* Return n if it's non-negative */
 }
