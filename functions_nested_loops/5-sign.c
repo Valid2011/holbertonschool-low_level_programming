0b3dec0e99b6fce90450732a9071fe6d68ce7d11
git config --global user.name Your <orujovvalid2020@gmail.com>
2024-11-17 18:10:16 +0400

    a

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,25 +2,25 @@
 
 /**
  * print_sign - prints the sign of a number.
- * @n: the integer to be checked
+ * @n: the number to check
  *
- * Return: 1 if n is greater than 0, 0 if n is 0, and -1 if n is less than 0
+ * Return: 1 if positive, 0 if zero, -1 if negative
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else
-    {
-        _putchar('-');
-        return (-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
