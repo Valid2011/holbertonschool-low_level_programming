25fe69c6d3d2c05ed6c8fb715ceb636f7632dadc
git config --global user.name Your <orujovvalid2020@gmail.com>
2024-11-17 18:08:12 +0400

    a

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -4,27 +4,59 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
     int r;
 
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    r = _abs(12);
-    printf("%d\n", r);
-    r = _abs(0x0F02);
-    printf("%d\n", r);
-    r = _abs(INT_MAX);
-    printf("%d\n", r);
-    r = _abs(INT_MIN);
-    printf("%d\n", r);
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(0xff); /* 0xff is equivalent to 255 in decimal */
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(99);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(INT_MAX);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(INT_MIN);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(-4);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
     return (0);
 }
