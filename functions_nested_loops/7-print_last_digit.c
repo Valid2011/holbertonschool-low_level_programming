05ae0c30232def37795a0dbde36a81f936d2deab
git config --global user.name Your <orujovvalid2020@gmail.com>
2024-11-19 17:21:12 +0400

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number.
- * @n: the number from which the last digit will be extracted.
- * 
- * Return: the last digit of the number.
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to extract the last digit from
+ *
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
     int last_digit;
 
-    if (n < 0)
-    {
-        n = -n;  /* Make the number positive if it is negative */
-    }
+    last_digit = n % 10;
+    if (last_digit < 0)
+        last_digit *= -1;
 
-    last_digit = n % 10;  /* Get the last digit of the number */
-    
-    _putchar('0' + last_digit);  /* Print the last digit */
-    
-    return (last_digit);  /* Return the last digit */
+    _putchar('0' + last_digit);
+    return (last_digit);
 }
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,9 +1,7 @@
 #include "main.h"
-#include <stdio.h>
-#include <limits.h>  /* To use INT_MAX and INT_MIN */
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -11,29 +9,10 @@ int main(void)
 {
     int r;
 
-    r = print_last_digit(98);  /* Should print 8 */
-    _putchar('0' + r);  /* Convert the result to a character and print */
-    _putchar('\n');
-
-    r = print_last_digit(0);   /* Should print 0 */
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
     _putchar('0' + r);
     _putchar('\n');
-
-    r = print_last_digit(-1024); /* Should print 4 */
-    _putchar('0' + r);
-    _putchar('\n');
-
-    r = print_last_digit(INT_MAX); /* Should print 7 (last digit of INT_MAX) */
-    _putchar('0' + r);
-    _putchar('\n');
-
-    r = print_last_digit(INT_MIN); /* Should print 8 (last digit of INT_MIN) */
-    _putchar('0' + r);
-    _putchar('\n');
-
-    r = print_last_digit(1);     /* Should print 1 */
-    _putchar('0' + r);
-    _putchar('\n');
-
     return (0);
 }
