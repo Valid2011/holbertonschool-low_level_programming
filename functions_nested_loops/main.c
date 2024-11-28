51a3bf5b6e6d7db224bd79037b4bb2d88f2e4aca
git config --global user.name Your <orujovvalid2020@gmail.com>
2024-11-28 14:55:49 +0400

    a

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -8,9 +8,10 @@
  */
 int main(void)
 {
-    int n;
-
-    n = add(89, 9);
-    printf("%d\n", n);
+    print_to_98(0);      /* Test case 1: Start from 0 */
+    print_to_98(98);     /* Test case 2: Start from 98 */
+    print_to_98(111);    /* Test case 3: Start from a number greater than 98 */
+    print_to_98(81);     /* Test case 4: Start from a number less than 98 */
+    print_to_98(-10);    /* Test case 5: Start from a negative number */
     return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int add(int a, int b);
+void print_to_98(int n);
 
 #endif /* MAIN_H */
